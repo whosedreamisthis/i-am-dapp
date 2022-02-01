8902698ca66a6de82fef70a8943c6f0cd3b236db
Dana Sharon <whosedreamisthis@gmail.com>
2022-02-01 09:59:18 -0800

    Updates

---
+++ precache-manifest.3751ac580e02297cc841d64b9402bf3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1440ace7501e58ad35709eab6a07a165",
+    "url": "/whosedreamisthis/i-am-dapp/index.html"
+  },
+  {
+    "revision": "08d8bda212ab54c3d611",
+    "url": "/whosedreamisthis/i-am-dapp/static/css/main.66f955a8.chunk.css"
+  },
+  {
+    "revision": "9481534bee815046e43d",
+    "url": "/whosedreamisthis/i-am-dapp/static/js/2.9e43f696.chunk.js"
+  },
+  {
+    "revision": "08d8bda212ab54c3d611",
+    "url": "/whosedreamisthis/i-am-dapp/static/js/main.f76f7ddb.chunk.js"
+  },
+  {
+    "revision": "7f6d1bd4001f851ee0ea",
+    "url": "/whosedreamisthis/i-am-dapp/static/js/runtime-main.1516228d.js"
+  }
+]);
